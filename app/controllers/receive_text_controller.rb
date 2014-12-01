aa05ae57c74dbb02da3c59695558bc31f16cdabb
Chase Merlin <chasemerlin@gmail.com>
2014-11-30 19:58:21 -0600

    create new evaluation is successful

--- app/controllers/receive_text_controller.rb
+++ app/controllers/receive_text_controller.rb
@@ -1,9 +1,10 @@
 class ReceiveTextController < ApplicationController
   def index
-    @message_body = params["Body"]
+    @message_body = params["Body"] if params["Body"]
     from_number = params["From"]
  
     new_evaluation = SelfEvaluation.new()
-    new_evaluation.comments = message_body
+    new_evaluation.comments = @message_body if @message_body
+    new_evaluation.save
   end
 end
