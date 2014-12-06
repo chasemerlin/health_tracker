44df4dd10a164277ef8466f5ac6f1f2786d020ef
Chase Merlin <chasemerlin@gmail.com>
2014-12-06 13:46:42 -0600

    return to stuffing entire text response in comments

--- app/controllers/receive_text_controller.rb
+++ app/controllers/receive_text_controller.rb
@@ -2,14 +2,12 @@ class ReceiveTextController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    message_body = params["Body"] if params["Body"]
-    mood_rating = message_body.split(",").first
-    energy_rating = message_body.split(",").last
+    @message_body = params["Body"] if params["Body"]
     from_number = params["From"]
  
     new_evaluation = SelfEvaluation.new()
-    new_evaluation.mood_rating = mood_rating
-    new_evaluation.energy_rating = energy_rating
+    new_evaluation.mood_rating = 1
+    new_evaluation.comments = @message_body if @message_body
 
     #Assign user here
 
