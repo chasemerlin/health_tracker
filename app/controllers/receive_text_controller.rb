ded62709493b843fab3da013cdd64896268e2ad2
Chase Merlin <chasemerlin@gmail.com>
2014-12-06 14:17:25 -0600

    assign user depending on phone number

--- app/controllers/receive_text_controller.rb
+++ app/controllers/receive_text_controller.rb
@@ -12,6 +12,11 @@ class ReceiveTextController < ApplicationController
     new_evaluation.energy_rating = energy_rating
 
     #Assign user here
+    if from_number == "+18134688604"
+      new_evaluation.user_id = 1
+    elsif from_number == "+18137167850"
+      new_evaluation.user_id = 2
+    end
     new_evaluation.save
   end
 end
